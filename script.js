648732c5814a92d2589c5048397369c3e98348b4
BenjaminANgafua <benjaminarkutl2017@gmail.com>
2021-04-04 18:33:37 +0000

    updating

--- script.js
+++ script.js
@@ -11,25 +11,32 @@ function questionsForm() {
     fetch(`https://kit-questions.glitch.me/question/${subject.value}/${number_of_questions.value}`)
         .then(response => response.json())
         .then(data => {
+
             /* converting data to arry */
+            let regular_data = Object.entries(data.questions)
+
+
             /* Looping through data */
-            let regular_data = Object.entries(data.questions);
+            ;
             regular_data.forEach((element, index) => {
                 questions = element[1].question; //Question to be answer
                 let option = element[1].options; //Option
                 correct_answer = element[1].answer; //answer correct answer
-                // /* Looping through option */
+
+                /* Looping through option */
                 let opt = Object.entries(option);
                 let option_value;
                 opt.forEach((value, index2) => {
-                    id = `opt${index}-${index2 +1}`
+                    id = `${element[0]}-${index2 +1}`
                     option_value += `
                     <li>
                         <label for="${id}">
-                            <input type="radio" name="qu-${index}" id="${id}" value="${value[0]}" >${value}</input> 
+                            <input type="radio" name="${element[0]}" id="${id}" value="${value[0]}" >${value}</input> 
                         </label>
                     </li>`
                 })
+
+                /* displaying in a DOM */
                 questions_field.insertAdjacentHTML("beforebegin",
                     `<div id="quiz-style"> 
                         <li><b> ${index + 1}. ${questions}</b></li> 
@@ -38,19 +45,40 @@ function questionsForm() {
                     </div>
                     `)
             });
+
+            /* SUBMIT FUNCTION */
             let sub_btn = document.querySelector("#button")
             sub_btn.addEventListener("click", () => {
-                let sel_ans = document.querySelectorAll("input[type='radio']")
-                for (let i = 0; i < sel_ans.length; i++) {
-                    if (sel_ans[i].checked) {
-                        // let select = sel_ans[i].value
-                        // if (select === correct_answer) {
-                        // opt_style.innerHTML.style = "backgroundColor: green"
-                        // }
-                        console.log(regular_data)
+                let radio_btn = document.querySelectorAll("input[type='radio']")
 
-                        let opt_style = document.querySelector("label").innerText
-                        console.log(opt_style)
+                /* Testing answer */
+                var tru;
+                let ans_data = data.questions
+
+                let ans_loop = Object.keys(ans_data);
+                ans_loop.forEach(elem => {
+                    tru = ans_data[elem].answer
+                    console.log('First answer', tru)
+                })
+
+                /* looping over radio button */
+                for (let i = 0; i < radio_btn.length; i++) {
+
+                    /* to see the checked box */
+                    if (radio_btn[i].checked) {
+                        let select = radio_btn[i].value
+
+                        console.log('SELECTED OPTION', select)
+                        console.log('answer', tru)
+
+                        /* Validating option and answer */
+                        if (select === tru) {
+                            console.log("correct")
+                        } else {
+                            if (select !== tru) {
+                                console.log("wrong")
+                            }
+                        }
                     }
                 }
             })
@@ -59,5 +87,15 @@ function questionsForm() {
     /* display */
     score_board.style.display = "block";
     document.getElementById("q-num").style.display = "none";
-    // document.getElementById("submit").style.display = "block";
 }
+
+/**
+ * 
+
+    if (select === correct_answer) {
+    opt_style.innerHTML.style = "backgroundColor: green"
+    }
+    let opt_style = document.querySelector("label").innerText
+    console.log(opt_style)
+*
+*/
